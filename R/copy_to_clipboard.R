db0b70786232ae005403816a52b6aaefed81470e
Meera Patel <patelm9@mskcc.org>
2020-04-08 00:08:15 -0400

    move clipboard functions to broca package

--- R/copy_to_clipboard.R
+++
@@ -1,8 +0,0 @@
-#' Copies dataframe copied to clipboard
-#' @export
-
-copy_to_clipboard <- function(dataframe) {
-                            clip <- pipe("pbcopy", "wb")
-                            write.table(dataframe, clip, sep = "\t", row.names = FALSE)
-                            close(clip)
-}
