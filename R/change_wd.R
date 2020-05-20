4f22da14de297ff9bf1c555cceb8e356d77fee38
Meera Patel <patelm9@mskcc.org>
2020-05-20 18:47:39 -0400

    add change_wd function

---
+++ R/change_wd.R
@@ -0,0 +1,29 @@
+#' Set a new wd while saving the previous one
+#' @description On the condition that the new_wd is not the same as the current wd, this function sets a new working directory while saving the previous wd as a `wd0` object. If the condition is not met, no new wd is set and a `wd0` object will not be returned to the global environment.
+#' @importFrom secretary typewrite
+#' @importFrom crayon italic
+#' @export
+
+
+change_wd <-
+    function(new_wd, save = TRUE) {
+        if (getwd() != new_wd) {
+                        if (save == TRUE) {
+
+                                assign("wd0",
+                                        getwd(),
+                                       envir = globalenv())
+                                secretary::typewrite(crayon::italic("wd0"), "object created.")
+
+                        }
+
+                        setwd(new_wd)
+                        secretary::typewrite(crayon::italic(new_wd), "set as new wd.")
+
+        } else {
+
+                        secretary::typewrite(crayon::italic(new_wd), "is current wd. No changes made.")
+
+        }
+
+    }
