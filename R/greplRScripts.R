2731f10134307eeb2e652d14524d9d7c0047a737
Meera Patel <patelm9@mskcc.org>
2020-08-02 14:58:41 -0400

    add greplRScripts

---
+++ R/greplRScripts.R
@@ -0,0 +1,29 @@
+#' Find R Scripts with a Pattern
+#' @description This function identifies R scripts in the R subdirectory in the working directory that contains the pattern given as the argument.
+#' @param pattern pattern passed as the pattern argument in the grepl function
+#' @importFrom readr read_file
+#' @export
+
+greplRScripts <-
+    function(pattern) {
+
+        FILES <- list.files(path = "R", pattern = "[.]R$", full.names = TRUE)
+
+        output <- vector()
+
+        for (FILE in FILES) {
+
+            input <- readr::read_file(FILE)
+
+            if (grepl(pattern = pattern, input)) {
+
+                output <-
+                        unique(c(output,
+                          FILE))
+            }
+
+        }
+
+        return(output)
+
+    }
