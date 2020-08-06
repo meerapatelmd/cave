4a88069c3c55d630792c1e017f311e4050be9b16
Meera Patel <patelm9@mskcc.org>
2020-08-06 03:12:47 -0400

    add createLogo function

---
+++ R/createLogo.R
@@ -0,0 +1,20 @@
+#' Create Package Logo
+#' @importFrom hexSticker sticker
+#' @export
+
+
+createLogo <-
+        function(sourceImg,
+                 package,
+                 file,
+                 white_around_sticker = TRUE,
+                 ...) {
+
+
+                hexSticker::sticker(sourceImg,
+                                    package=package,
+                                    filename = file,
+                                    white_around_sticker = white_around_sticker,
+                                    ...)
+
+        }
