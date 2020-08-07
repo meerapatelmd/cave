fc080da45ed3944fc305643ea1b845a65bcb3759
Meera Patel <patelm9@mskcc.org>
2020-08-07 00:34:42 -0400

    add shortcute

---
+++ data-raw/shortcuts.R
@@ -0,0 +1,4 @@
+## code to prepare `shortcuts` dataset goes here
+
+readmeLogoShortcut <- '# {PACKAGE_NAME} <img src="man/figures/logo.png" align="right" alt="" width="120" />'
+usethis::use_data(readmeLogoShortcut, overwrite = TRUE)
