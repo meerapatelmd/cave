22bc54b70545a320d4c4b7a0ff14a58c10064b66
Meera Patel <patelmeeray@gmail.com>
2020-11-17 13:01:53 -0500

    combine files

--- R/make.R
+++ R/make.R
@@ -1,3 +1,18 @@
+#' Parse and evaluate a string
+#' @description This is the counterpart function of vector_to_string in this package.
+#' @export
+
+
+string_to_vector <-
+    function(string) {
+        parsed_expr <- rlang::parse_expr(string)
+        return(eval(parsed_expr))
+    }
+
+
+
+
+
 #' Convert a vector of values into a string
 #' @description This function allows to store values neatly in an Excel or other dataframe for summary or record-keeping, but is in a format that can be readily parsed back to an expression if need be. This is important in cases where the names of columns want to be saved for a dataframe in a particular script to reference back or to QA later on. The string can be converted back to a vector by its counterpart function string_to_vector() in this package.
 #' @return a parseable string of length one
@@ -21,3 +36,8 @@ vector_to_string <-
         return(espresso)
 
     }
+
+
+
+
+
