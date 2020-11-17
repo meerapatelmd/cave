dbde3ee5f0b85315d6559ff2f49bca2aa479a696
Meera Patel <patelmeeray@gmail.com>
2020-11-17 14:41:14 -0500

    deprecated functions

--- R/deprecated.R
+++ R/deprecated.R
@@ -276,3 +276,165 @@ replace_ext <-
 
 
 
+
+
+
+
+
+#' Returns the script step number
+#' @description returns the first 2 digits of the present R script filename
+#' @importFrom stringr str_replace_all
+#' @export
+
+
+script_step_number <-
+    function() {
+        return(stringr::str_replace_all(strip_fn(present_script_path()), "([0-9]{2})(.*$)", "\\1"))
+    }
+
+
+
+
+
+
+
+
+
+
+#' Interactively select a file
+#' @description Interactively select a file based on a menu printed into a console.
+#' @importFrom secretary typewrite
+#' @importFrom tibble tibble
+#' @export
+
+select_file <-
+    function(dir, new_obj_name = NULL, full_path = FALSE) {
+
+            .Deprecated()
+            fns <-
+                    dir %>%
+                    list.files()
+
+            print(tibble::tibble(Index = paste0("[", 1:length(fns), "]"),
+                        Files = fns))
+
+            secretary::typewrite("Please select a file by Index.")
+            answer <- readline("Selection: ")
+            answer <- as.integer(answer)
+            if (is.null(new_obj_name)) {
+                    if (full_path == FALSE) {
+                            return(fns[answer])
+                    } else {
+                            return(paste0(dir, "/", fns[answer]))
+                    }
+            } else {
+                    if (full_path == FALSE) {
+                            assign(new_obj_name,
+                                   value = fns[answer],
+                                   envir = globalenv())
+                    } else {
+                            assign(new_obj_name,
+                                   value = paste0(dir, "/", fns[answer]),
+                                   envir = globalenv())
+                    }
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+#' @title Function FileName
+#' @description
+#' This function compares the name of the file in the R subdir within the working directory and with the name of the function within the file.
+#' @return
+#' dataframe with `fileName` and `functionName` columns
+#' @seealso
+#'  \code{\link[purrr]{map}},\code{\link[purrr]{set_names}}
+#'  \code{\link[readr]{read_lines}}
+#'  \code{\link[stringr]{str_remove}}
+#'  \code{\link[rubix]{vector_to_tibble}}
+#'  \code{\link[dplyr]{bind}},\code{\link[dplyr]{mutate}}
+#' @rdname FunctionFileName
+#' @export
+#' @importFrom purrr map set_names
+#' @importFrom readr read_lines
+#' @importFrom stringr str_remove_all
+#' @importFrom tibble tibble
+#' @importFrom dplyr bind_rows mutate %>%
+
+FunctionFileName  <-
+        function() {
+
+                .Deprecated()
+                new_col <- "functionName"
+
+                list.files("R", full.names = TRUE) %>%
+                        purrr::map(readr::read_lines) %>%
+                        purrr::map(function(x) grep(pattern = "<-",
+                                                    x,
+                                                    value = TRUE)[1]) %>%
+                        purrr::map(function(x) stringr::str_remove_all(x, "[ ]{0,}[<]{1}[-]{1}[ ]{0,1}$")) %>%
+                        purrr::set_names(list.files("R")) %>%
+                        purrr::map(function(x) tibble::tibble(`:=`(!!new_col, x))) %>%
+                        dplyr::bind_rows(.id = "fileName") %>%
+                        dplyr::mutate(fileName = stringr::str_remove_all(fileName, pattern = "[.]{1}R$"))
+        }
+
+
+
+
+
+
+
+
+
+
+#' @title Duplicate Functions
+#' @seealso
+#'  \code{\link[purrr]{map}},\code{\link[purrr]{set_names}}
+#'  \code{\link[readr]{read_lines}}
+#'  \code{\link[stringr]{str_remove}}
+#'  \code{\link[rubix]{vector_to_tibble}}
+#'  \code{\link[dplyr]{bind}},\code{\link[dplyr]{mutate}},\code{\link[dplyr]{group_by}},\code{\link[dplyr]{summarise}},\code{\link[dplyr]{arrange}},\code{\link[dplyr]{filter}}
+#' @rdname duplicateFunctions
+#' @export
+#' @importFrom purrr map set_names
+#' @importFrom readr read_lines
+#' @importFrom stringr str_remove_all
+#' @importFrom tibble tibble
+#' @importFrom dplyr bind_rows mutate group_by summarize arrange ungroup filter %>%
+
+duplicateFunctions <-
+        function() {
+
+          .Deprecated()
+
+                new_col <- "functionName"
+
+                list.files("R", full.names = TRUE) %>%
+                        purrr::map(readr::read_lines) %>%
+                        purrr::map(function(x) grep(pattern = "<-",
+                                                    x,
+                                                    value = TRUE)[1]) %>%
+                        purrr::map(function(x) stringr::str_remove_all(x, "[ ]{0,}[<]{1}[-]{1}[ ]{0,1}$")) %>%
+                        purrr::set_names(list.files("R")) %>%
+                        purrr::map(function(x) tibble::tibble(`:=`(!!new_col, x))) %>%
+                        dplyr::bind_rows(.id = "fileName") %>%
+                        dplyr::mutate(fileName = stringr::str_remove_all(fileName, pattern = "[.]{1}R$")) %>%
+                        dplyr::group_by(functionName) %>%
+                        dplyr::summarize(n = n(), .groups = "drop") %>%
+                        dplyr::arrange(n) %>%
+                        dplyr::ungroup() %>%
+                        dplyr::filter(n > 1)
+        }
+
+
+
+
+
