80dd1e98a3c2d6544b790072cad06a0e32096183
Meera Patel <patelmeeray@gmail.com>
2020-11-30 14:07:56 -0500

    deprecate `make` functions

--- R/deprecated.R
+++ R/deprecated.R
@@ -426,3 +426,71 @@ duplicateFunctions <-
       dplyr::ungroup() %>%
       dplyr::filter(n > 1)
   }
+
+#' Remove the file ext from fn
+#' @param full_fn filename
+#' @importFrom stringr str_remove
+#' @export
+
+strip_fn <-
+  function(full_fn, rm_ext = TRUE, upto_nchar_file_ext = 4, rm_path = TRUE) {
+    .Deprecated("skin_file")
+
+    x <- full_fn
+    if (rm_ext == TRUE) {
+      x <- str_remove(full_fn, paste0(
+        "[.]{1}[a-zA-Z]{1,",
+        upto_nchar_file_ext,
+        "}$"
+      ))
+    }
+
+    if (rm_path == TRUE) {
+      x <- basename(x)
+    }
+
+    return(x)
+  }
+
+
+
+#' Parse and evaluate a string
+#' @description (Deprecated) This is the counterpart function of vector_to_string in this package.
+#' @export
+
+
+string_to_vector <-
+  function(string) {
+    .Deprecated()
+    parsed_expr <- rlang::parse_expr(string)
+    return(eval(parsed_expr))
+  }
+
+
+
+
+
+#' Convert a vector of values into a string
+#' @description (Deprecated) This function allows to store values neatly in an Excel or other dataframe for summary or record-keeping, but is in a format that can be readily parsed back to an expression if need be. This is important in cases where the names of columns want to be saved for a dataframe in a particular script to reference back or to QA later on. The string can be converted back to a vector by its counterpart function string_to_vector() in this package.
+#' @return a parseable string of length one
+#' @importFrom stringr str_replace_all
+#' @export
+
+
+vector_to_string <-
+  function(vector_to_string) {
+
+    .Deprecated()
+    vector_to_string <- as.character(vector_to_string)
+
+    # Enquoting the strings
+    vector_to_string <- paste0("'", vector_to_string, "'")
+
+    # Adding commas and bordering with c()
+    espresso <- paste0("c(", paste(vector_to_string, collapse = ", "), ")")
+
+    ## For vectors where values were already quoted, the quotes are replaced with single quotes
+    expresso <- stringr::str_replace_all(espresso, "\"", "'")
+
+    return(espresso)
+  }
